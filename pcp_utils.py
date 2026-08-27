f2c8c82c11e00b27fbc1019a1feb6c6a4a58c464
weiguihua2 <984323595@qq.com>
2026-08-27 14:14:46 +0800

    test

--- pcp_utils.py
+++ pcp_utils.py
@@ -1,6 +1,4 @@
-curl -X POST http://80.5.17.112:10086/start_profile
-sleep 3
-curl -X POST http://80.5.17.112:10086/stop_profile
+
 
 from torch_npu.profiler.profiler import analyse
 analyse("/mnt/share/w00504341/profiling")
